0281bc1a5ed53ea7164d865b04e5fc16cf9fbc50
Thijs D'Hulster <thijs.dhulster@gmail.com>
2013-04-27 16:27:03 +0200

    Basic setup Angular application with Yeoman

---
+++ application/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('twitstreamApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

6d062d88680b45585fe16598520d2a2e1cc9a4b6
Voles <niels.dequeker@gmail.com>
2013-05-19 22:23:51 +0200

    Case insensitive search

--- app/scripts/filters/highlight.js
+++ app/scripts/filters/highlight.js
@@ -4,7 +4,7 @@ app.filter('highlight', [function ()
 {
 	return function(text, keyword)
 	{
-		var rgxp = new RegExp(keyword, 'g');
+		var rgxp = new RegExp(keyword, 'gi');
 		var repl = '<span class="label label-warning">' + keyword + '</span>';
 		return text.replace(rgxp, repl);
 	};
